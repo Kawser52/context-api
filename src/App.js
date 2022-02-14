fe6fc94f96361b466624a1f21daa2ebbe8a84035
TechDev-CEO <shoriful521399@gmail.com>
2022-02-14 12:46:36 +0600

    Just Applied

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import GrandFather from './component/GranFather/GrandFather';
+import { useContext } from 'react';
 
+export const RingContext = useContext('ring');
+
+const ornaments = 'Diamond Ring'
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RingContext.Provider value = {ornaments}>
+      <div className="App">
+            <GrandFather></GrandFather>
+      </div>
+    </RingContext.Provider>
+    
   );
 }
 
